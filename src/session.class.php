8c78112fe8a6b67726dd4c47001f76205534e239
Nilton Teixeira <nilton@dwk.com.br>
2015-06-11 13:26:25 -0300

    Changed copyright in file beginning

--- src/session.class.php
+++ src/session.class.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2010 Richard Castera
+ * Copyright (c) 2015 Nilton Teixeira
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
